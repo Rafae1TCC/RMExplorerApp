62952973a897e4b45060acb5976201a4dfcdab3a
Rafael Cabrera <rafael.cabrera32@uabc.edu.mx>
2026-08-22 09:15:36 -0700

    Initial Commit

---
+++ RMExplorerApp/RMExplorerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RMExplorerAppApp.swift
+//  RMExplorerApp
+//
+//  Created by Rafael Cabrera on 8/22/26.
+//
+
+import SwiftUI
+
+@main
+struct RMExplorerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
